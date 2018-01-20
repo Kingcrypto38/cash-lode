ce0bebc75ed72d3773c46a333bfc560d10e7d66a
Juan Carlos Medina <juanca.med@gmail.com>
2018-01-19 16:59:04 -0800

    Ensure cache directory exists

--- src/pitch.js
+++ src/pitch.js
@@ -1,2 +1,11 @@
+const fs = require('fs');
+
 module.exports = function pitch(modulePath) {
+  const cachePath = path.join(this.options.context, this.query.cacheDirectory);
+
+  if (!fs.existsSync(cachePath)) {
+    fs.mkdirSync(cachePath);
+  }
+
+  const cachedFileNames = this.fs.fileSystem.readdirSync(cachePath);
 }
