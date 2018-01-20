41559f507bd60bf221941babf84ec983205f9d16
Juan Carlos Medina <juanca.med@gmail.com>
2018-01-19 16:57:08 -0800

    Set up cash-lode e2e configuration and gitignore its cache directory

--- e2e-test/webpack.config.js
+++ e2e-test/webpack.config.js
@@ -1,5 +1,20 @@
 module.exports = {
   entry: './e2e-test/index.js',
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: require.resolve('../index.js'),
+          options: {
+            cacheDirectory: './e2e-test/.cash-lode',
+          },
+        },
+      },
+    ],
+  },
+
   output: {
     filename: 'e2e-test/bundle.js',
   },
