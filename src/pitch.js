6bb8f4f7682753f98ac58172008bc7d2981a6bc3
Juan Carlos Medina <juanca.med@gmail.com>
2018-01-19 17:01:55 -0800

    Hash raw file and check if its cached file is in the cache directory
    At the moment, this is all done in the pitch phase which could be slow for every file. There is some thought at front-loading this work with a plugin or something like that.
    
    Ideally, this function should only be checking a lookup. If it is there, it reads the cached file and processes it. Otherwise, nothing.

--- src/pitch.js
+++ src/pitch.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
+const md5 = require('md5');
+const path = require('path');
 
 module.exports = function pitch(modulePath) {
+  console.log('Processing', this.resourcePath);
   const cachePath = path.join(this.options.context, this.query.cacheDirectory);
 
   if (!fs.existsSync(cachePath)) {
@@ -8,4 +11,19 @@ module.exports = function pitch(modulePath) {
   }
 
   const cachedFileNames = this.fs.fileSystem.readdirSync(cachePath);
+
+  // Normally, there already exists a map for each resource to its hashed key.
+  // However, first version will compute hash.
+  const fileContent = this.fs.fileSystem.readFileSync(this.resourcePath);
+  const fileKey = md5(fileContent);
+
+  if (cachedFileNames.includes(fileKey)) {
+    const cachedFilePath = path.join(cachePath, fileKey);
+    const cachedFileContent = this.fs.fileSystem.readFileSync(cachedFilePath);
+    console.log('Found in cache: ', cachedFileContent);
+    return cachedFileContent;
+  } else {
+    console.log('Did not find in cache.');
+    return;
+  }
 }
