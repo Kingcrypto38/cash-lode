0c1136964b6fc3ddaa0460ae7c74c9513b5c8d08
Juan Carlos Medina <juanca.med@gmail.com>
2018-01-20 11:30:58 -0800

    Write to cache with basic data

--- src/loader.js
+++ src/loader.js
@@ -1,9 +1,22 @@
+const fs = require('fs');
 const md5 = require('md5');
+const path = require('path');
 
 const getCachePath = require('./utils/cache-directory-path.js');
 
 module.exports = function loader(source) {
   console.log('Caching', this.resourcePath);
   const cachePath = getCachePath(this.options.context, this.query.cacheDirectory);
+  const cachedFileNames = this.fs.fileSystem.readdirSync(cachePath);
+
+  const fileContent = this.fs.fileSystem.readFileSync(this.resourcePath);
   const fileKey = md5(fileContent);
+
+  const cachedFilePath = path.join(cachePath, fileKey);
+  const cacheFileContent = JSON.stringify({
+    source: source,
+  });
+  fs.writeFileSync(cachedFilePath, cacheFileContent);
+
+  return source;
 };
